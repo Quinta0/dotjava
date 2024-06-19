c7f51ffc36869f50d3ed1d40f754f1813954005d
Quinta <0pietroquintavalle0@gmail.com>
2024-06-20 00:13:42 +0200

    Fixing

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,8 @@
-import {JSX, SVGProps} from "react";
+import { JSX, SVGProps } from "react";
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -23,32 +22,32 @@ export default function Footer() {
                                 <DialogDescription>
                                     <p className="font-bold">Copyright (c) 2024 Quintavalle Pietro</p>
                                     <p className="mt-2">
-                                    Permission is hereby granted, free of charge, to any person obtaining a copy
-                                    of this software and associated documentation files (the "Software"), to deal
-                                    in the Software without restriction, including without limitation the rights
-                                    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                                    copies of the Software, and to permit persons to whom the Software is
-                                    furnished to do so, subject to the following conditions:
+                                        Permission is hereby granted, free of charge, to any person obtaining a copy
+                                        of this software and associated documentation files (the &quot;Software&quot;), to deal
+                                        in the Software without restriction, including without limitation the rights
+                                        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                                        copies of the Software, and to permit persons to whom the Software is
+                                        furnished to do so, subject to the following conditions:
                                     </p>
                                     <p>
-                                    The above copyright notice and this permission notice shall be included in all
-                                    copies or substantial portions of the Software.
+                                        The above copyright notice and this permission notice shall be included in all
+                                        copies or substantial portions of the Software.
                                     </p>
                                     <p className="mt-2">
-                                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                                    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                                    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                                    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                                    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                                    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                                    SOFTWARE.
+                                        THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                                        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                                        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                                        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                                        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                                        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                                        SOFTWARE.
                                     </p>
                                 </DialogDescription>
                             </DialogHeader>
                         </DialogContent>
                     </Dialog>
 
-                <a href="https://github.com/Quinta0" target="_blank" rel="noopener noreferrer"
+                    <a href="https://github.com/Quinta0" target="_blank" rel="noopener noreferrer"
                        className="hover:underline">
                         <GitHubIcon className="w-6 h-6"/>
                     </a>
@@ -64,8 +63,6 @@ export default function Footer() {
         </footer>
     )
 }
-
-
 
 function GitHubIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
     return (
